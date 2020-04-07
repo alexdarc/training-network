07e8587c60732c0621921d94c0de6bcea6981c04
Alexander Kim <alex.dark.kim@gmail.com>
2020-04-07 15:09:19 +0300

    init

---
+++ RRNetworkConsoleClient/ConsoleClient.cs
@@ -0,0 +1,91 @@
+﻿namespace RRNetworkConsole
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    internal class ConsoleClient
+    {
+        public static void Main(
+            string[] args)
+        {
+            var clientSettings = InitializeClientSettings();
+            TestConnection(clientSettings: clientSettings);
+
+            while (true)
+            {
+                Console.Write(value: "Enter message (Enter to exit): ");
+                var message = Console.ReadLine();
+
+                if (message?.Length == 0)
+                {
+                    break;
+                }
+
+                Console.WriteLine(value: "Sending...");
+                SendMessage(
+                    clientSettings: clientSettings,
+                    message: message);
+                Console.WriteLine(format: "Message '{0}' sent", arg0: message);
+            }
+        }
+
+        private static void SendMessage(
+            ClientSettings clientSettings,
+            string message)
+        {
+            var client = new TcpClient(
+                hostname: clientSettings.hostname,
+                port: clientSettings.port);
+            var stream = client.GetStream();
+            var bytesToSend = Encoding.UTF8.GetBytes(s: message);
+
+            stream.Write(
+                buffer: bytesToSend,
+                offset: 0,
+                size: bytesToSend.Length);
+            client.Close();
+        }
+
+        private static ClientSettings InitializeClientSettings()
+        {
+            Console.WriteLine(value: "Enter hostname: ");
+            var host = Console.ReadLine();
+            Console.WriteLine(value: "Enter port: ");
+            var port = Console.ReadLine();
+
+            if (host == null || port == null)
+            {
+                throw new Exception(message: "Hostname or port are incorrect");
+            }
+
+            return new ClientSettings(
+                hostname: host,
+                port: int.Parse(s: port));
+        }
+
+        private static void TestConnection(ClientSettings clientSettings)
+        {
+            var client = new TcpClient(
+                hostname: clientSettings.hostname,
+                port: clientSettings.port);
+            client.Close();
+        }
+
+        private class ClientSettings
+        {
+            public ClientSettings(
+                string hostname,
+                int port)
+            {
+                this.hostname = hostname;
+                this.port = port;
+            }
+
+            public string hostname { get; }
+
+            public int port { get; }
+        }
+    }
+}
---
+++ RRNetworkConsoleServer/ConsoleServer.cs
@@ -0,0 +1,86 @@
+﻿namespace RRNetworkConsoleServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    internal class ConsoleServer
+    {
+        public static void Main(
+            string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                var portSettings = InitializePortSettings();
+                server = new TcpListener(
+                    localaddr: IPAddress.Any,
+                    port: portSettings.port);
+
+                Console.WriteLine(value: "Listening...");
+                server.Start();
+
+                while (true)
+                {
+                    var client = server.AcceptTcpClient();
+
+                    var stream = client.GetStream();
+                    var buffer = new byte[client.ReceiveBufferSize];
+
+                    var bytesRead = stream.Read(
+                        buffer: buffer,
+                        offset: 0,
+                        size: client.ReceiveBufferSize);
+
+                    var receivedMessage = Encoding.UTF8.GetString(
+                        bytes: buffer,
+                        index: 0,
+                        count: bytesRead);
+
+                    if (receivedMessage.Length > 0)
+                    {
+                        Console.WriteLine(
+                            format: "Received: {0}",
+                            arg0: receivedMessage);
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(value: e.Message);
+            }
+            finally
+            {
+                server?.Stop();
+            }
+        }
+
+        private static PortSettings InitializePortSettings()
+        {
+            Console.WriteLine(value: "Enter host port: ");
+            var port = Console.ReadLine();
+
+            if (port == null)
+            {
+                throw new Exception(message: "Enter correct values");
+            }
+
+            return new PortSettings(
+                port: int.Parse(s: port));
+        }
+
+        private class PortSettings
+        {
+            public PortSettings(
+                int port)
+            {
+                this.port = port;
+            }
+
+            public int port { get; }
+        }
+    }
+}
